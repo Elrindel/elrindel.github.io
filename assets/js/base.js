1e0642967d5f06a4f7968af977264ece8d2335d0
Flob <flob@flob.ovh>
2020-05-02 11:25:11 +0200

    Remove jQuery + clean JS

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,9 +1,3 @@
 function hasClass(s,a){return s.classList?s.classList.contains(a):!!s.cl.match(new RegExp("(\\s|^)"+a+"(\\s|$)"))}function addClass(s,a){s.classList?s.classList.add(a):hasClass(s,a)||(s.cl+=" "+a)}function removeClass(s,a){if(s.classList)s.classList.remove(a);else if(hasClass(s,a)){var l=new RegExp("(\\s|^)"+a+"(\\s|$)");s.cl=s.cl.replace(l," ")}}
 
-$('document').ready(function(){
-    $('.navicon i').click(function(e) {
-        e.preventDefault();
-
-        $('.navbar').slideToggle('fast');
-    });
-});
+document.querySelector(".navicon i").onclick=function(e){var o=document.querySelector(".navbar");(hasClass(o,"show")?removeClass:addClass)(o,"show")};
