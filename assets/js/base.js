640a056b3bbe5a8c1c8c71b54b13bff4d5c595f8
Flob <flob@flob.ovh>
2020-07-18 17:24:45 +0200

    Article Arduino Flash Programmer + Lightbox

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,3 +1,6 @@
 function hasClass(s,a){return s.classList?s.classList.contains(a):!!s.cl.match(new RegExp("(\\s|^)"+a+"(\\s|$)"))}function addClass(s,a){s.classList?s.classList.add(a):hasClass(s,a)||(s.cl+=" "+a)}function removeClass(s,a){if(s.classList)s.classList.remove(a);else if(hasClass(s,a)){var l=new RegExp("(\\s|^)"+a+"(\\s|$)");s.cl=s.cl.replace(l," ")}}
 
 document.querySelector(".navicon i").onclick=function(e){var o=document.querySelector(".navbar");(hasClass(o,"show")?removeClass:addClass)(o,"show")};
+
+var imglightbox = new SimpleLightbox({elements:'img.lightbox',urlAttribute:'src'});
+var alightbox = new SimpleLightbox({elements:'a.lightbox'});
